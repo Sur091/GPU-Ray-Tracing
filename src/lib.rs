181f4d293ccdbdc890251754f6c3ee353ba975cd
Suraj Acharya <acharyasuraj091@gmail.com>
2025-02-23 20:47:17 -0500

    A functioning screen. The material resizes with the screen

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 
 use bevy::prelude::*;
 use bevy::sprite::Material2dPlugin;
+use bevy::window::{PrimaryWindow, WindowResized};
 
 mod custom_material;
 use custom_material::CustomMaterial;
@@ -13,21 +14,52 @@ pub fn run() {
             Material2dPlugin::<CustomMaterial>::default(),
         ))
         .add_systems(Startup, setup)
+        .add_systems(Update, resize_rectangle)
         .run();
 }
+
+#[derive(Component)]
+struct ResizableRectangle;
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<CustomMaterial>>,
+    windows: Query<&Window, With<PrimaryWindow>>,
 ) {
+    // Ensure that there is exactly one primary window
+    let window = windows.single();
+    let (width, height) = (window.width(), window.height());
+    let rectangle = Rectangle::new(width, height);
     commands.spawn((
-        Mesh2d(meshes.add(Rectangle::default())),
+        Mesh2d(meshes.add(rectangle)),
         MeshMaterial2d(materials.add(CustomMaterial {
             color: LinearRgba::BLUE,
         })),
-        Transform::default().with_scale(Vec3::splat(256.)),
+        Transform::default(),
+        ResizableRectangle,
     ));
 
     commands.spawn(Camera2d);
+}
+
+fn resize_rectangle(
+    mut meshes: ResMut<Assets<Mesh>>,
+    windows: Query<&Window, With<PrimaryWindow>>,
+    mut query: Query<&mut Mesh2d, With<ResizableRectangle>>,
+    resize_events: EventReader<WindowResized>,
+) {
+    // Check if there is a resize event
+    if resize_events.is_empty() {
+        return;
+    }
+
+    // Get the new window dimensions
+    let window = windows.single();
+    let (width, height) = (window.width(), window.height());
+
+    for mut resizable_rectangle in query.iter_mut() {
+        let rectangle = Rectangle::new(width, height);
+        *resizable_rectangle = Mesh2d(meshes.add(rectangle));
+    }
 }

c5baf222a7b21d1906f57a00bf51ef4a95c6ce3f
Suraj Acharya <acharyasuraj091@gmail.com>
2025-03-03 20:17:12 -0500

    Adding two types of light and two spheres in Bevy

--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,12 @@
 // A shader in Bevy
 
 use bevy::prelude::*;
-use bevy::sprite::Material2dPlugin;
-use bevy::window::{PrimaryWindow, WindowResized};
 
-mod custom_material;
-use custom_material::CustomMaterial;
+mod scene;
 
 pub fn run() {
     App::new()
-        .add_plugins((
-            DefaultPlugins,
-            Material2dPlugin::<CustomMaterial>::default(),
-        ))
-        .add_systems(Startup, setup)
-        .add_systems(Update, resize_rectangle)
+        .add_plugins((DefaultPlugins,))
+        .add_systems(Startup, scene::setup_scene)
         .run();
-}
-
-#[derive(Component)]
-struct ResizableRectangle;
-
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<CustomMaterial>>,
-    windows: Query<&Window, With<PrimaryWindow>>,
-) {
-    // Ensure that there is exactly one primary window
-    let window = windows.single();
-    let (width, height) = (window.width(), window.height());
-    let rectangle = Rectangle::new(width, height);
-    commands.spawn((
-        Mesh2d(meshes.add(rectangle)),
-        MeshMaterial2d(materials.add(CustomMaterial {
-            color: LinearRgba::BLUE,
-        })),
-        Transform::default(),
-        ResizableRectangle,
-    ));
-
-    commands.spawn(Camera2d);
-}
-
-fn resize_rectangle(
-    mut meshes: ResMut<Assets<Mesh>>,
-    windows: Query<&Window, With<PrimaryWindow>>,
-    mut query: Query<&mut Mesh2d, With<ResizableRectangle>>,
-    resize_events: EventReader<WindowResized>,
-) {
-    // Check if there is a resize event
-    if resize_events.is_empty() {
-        return;
-    }
-
-    // Get the new window dimensions
-    let window = windows.single();
-    let (width, height) = (window.width(), window.height());
-
-    for mut resizable_rectangle in query.iter_mut() {
-        let rectangle = Rectangle::new(width, height);
-        *resizable_rectangle = Mesh2d(meshes.add(rectangle));
-    }
 }
---
+++ src/scene.rs
@@ -0,0 +1,72 @@
+use std::f32::consts::PI;
+
+use bevy::color::palettes::css::*;
+use bevy::pbr::CascadeShadowConfigBuilder;
+use bevy::prelude::*;
+
+mod camera;
+
+// #[derive(Resource, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+// struct Scene;
+
+// Set up the scene
+pub fn setup_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Spawn a sphere by default
+    commands.spawn((
+        Mesh3d(meshes.add(Sphere::new(0.5).mesh().uv(32, 18))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: LIMEGREEN.into(),
+            ..default()
+        })),
+        Transform::from_xyz(0.0, 1.0, 1.5),
+    ));
+    
+    // Spawn a big sphere
+    commands.spawn((
+        Mesh3d(meshes.add(Sphere::new(10.5).mesh().uv(32, 18))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: SKY_BLUE.into(),
+            ..default()
+        })),
+        Transform::from_xyz(0.0, 1.0, -13.5),
+    ));
+
+    // ambient light
+    commands.insert_resource(AmbientLight {
+        color: SKY_BLUE.into(),
+        brightness: 100.0,
+    });
+
+    // directional 'sun' light
+    commands.spawn((
+        DirectionalLight {
+            illuminance: light_consts::lux::OVERCAST_DAY,
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform {
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            rotation: Quat::from_rotation_x(-PI / 4.),
+            ..default()
+        },
+        // The default cascade config is designed to handle large scenes.
+        // As this example has a much smaller world, we can tighten the shadow
+        // bounds for better visual quality.
+        CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 4.0,
+            maximum_distance: 10.0,
+            ..default()
+        }
+        .build(),
+    ));
+
+    // camera
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+}
